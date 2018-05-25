c76092faa8011f673e0583df0fa80c8e0c6ad795
Oleg Dzhimiev <oleg@elphel.com>
2018-05-25 14:42:05 -0600

    lost license header

--- imagej_tiff.py
+++ imagej_tiff.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python3
 
+'''
+/**
+ * @file imagej_tiff.py
+ * @brief open multi layer tiff files, display layers and parse meta data
+ * @par <b>License</b>:
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+'''
+
+__copyright__ = "Copyright 2018, Elphel, Inc."
+__license__   = "GPL-3.0+"
+__email__     = "oleg@elphel.com"
+
+
 from PIL import Image
 import xml.etree.ElementTree as ET
 import numpy as np
